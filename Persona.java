ce0fdf1027c188ca5a6fe870acb9f744ac81575f
Diego Zepeda <107309383+DiegoCoder22@users.noreply.github.com>
2022-08-26 22:10:48 -0400

    Persona.java

--- Persona.java
+++ Persona.java
@@ -1,11 +1,27 @@
 package demoo.Modelos;
 public class Persona {
     // [Acceso] [estatico] [nombre variable];
-    public String Nombres;
+    private String Nombres;
     public String Apellidos;
     public String Rut;
     public float Altura;
     public float Peso;
+    
+    /* Metodo para poder comunicar algun atributo que este en privado */
+    /* Metodo GETTER */
+    public String getNombres(){
+        return this.Nombres;        
+    }
+    /* Metodo SETTER */
+    /* Cuando tengo atributos provados, con esto, puedo hacer una
+    excepcion para poder trabajar con el */
+    public void setNombres(String nombres, Sting Apellido, String Rut){
+        this.Nombres = nombres;
+    }
+    
+    public void setNombres(String nombres){
+        this.Nombres = nombres;
+    }
 }
 /*
 CREATE TABLE Persona (
